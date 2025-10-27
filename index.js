d546b3a076b4983bcf5a4a6295386e50c4313f36
Maria <mariaacllara2001@gmail.com>
2025-10-27 19:27:46 -0300

    alteração no aba principal

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const app = express(); //iniciando o express
 
 //criando a rota inicial
 app.get("/", function(req,res){
-    res.send("<h1>Bem vindo ao site da Maria Clara!!</h1>");
+    res.send("<h1>Bem vindo ao site da Maria!</h1>");
 })
 
 //rota do cadastro de produtos
 app.get("/produtos", function(req,res){
-    res.send("<h1>Lista de Produtos!</h1>");
+    res.send("<h1>Lista de produtos!</h1>");
 })
 
 //rota com parametro 
